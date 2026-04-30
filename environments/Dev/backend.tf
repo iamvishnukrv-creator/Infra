5d83e531905d40ccd2e367f4d6985afa08a42f53
vishnu <iamvishnukrv@gmail.com>
2026-04-30 09:16:48 +0530

    Added Dev in dev branch

---
+++ environments/Dev/backend.tf
@@ -0,0 +1,16 @@
+modules "vpc" {
+    source = "../../Modules/vpc"
+    vpc_cidr_block = "10.0.0.0/16"
+    pub_cidr-block = "10.0.1.0/24"
+    subnet_az      = "${var.aws_region}a"
+    env            ="dev" 
+}
+
+modules "ec2" {
+    source = "../../Moddules.ec2"
+    ec2_ami_id = "ami-02dd44faa40720bb8"
+    ec2_instance_type = "t3.micro"
+    ec2_instance_count = 1
+     subnet_id = module.vpc.subnet_id
+     env   ="dev"
+}  
---
+++ environments/Dev/variable.tf
@@ -0,0 +1,5 @@
+variable aws_region {
+  type        = string
+  default     = "ap-southeast-1"
+  description = "aws_region"
+}
