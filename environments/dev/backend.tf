6a74303ea35072aa9c784f7f022010e9094211e8
vishnu <iamvishnukrv@gmail.com>
2026-04-30 12:36:43 +0530

    removed s3 bucket

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -5,11 +5,7 @@ terraform {
       version = "4.67.0"
     }
   }
-  backend "s3" {
-    bucket = "lwm-terraform-bucket"
-    key    = "environments/dev/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
+ 
 }
 
 provider "aws" {
